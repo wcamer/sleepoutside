15c5ff5c8a563272d97ef154be2d9f388cd1f4b9
William C <104468618+wcamer@users.noreply.github.com>
2024-03-01 13:46:31 -0700

    Update product.js
    Updated the "addProductToCart" function for the solution

--- src/js/product.js
+++ src/js/product.js
@@ -1,11 +1,29 @@
 import { setLocalStorage } from "./utils.mjs";
+import { getLocalStorage } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 
 const dataSource = new ProductData("tents");
 
+//og code below
+// function addProductToCart(product) {
+//   setLocalStorage("so-cart", product);
+// }
+
+//solution
 function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
+  let newArray = [];
+  let array = getLocalStorage("so-cart");
+  if (array == null){
+    array = newArray;
+    array.push(product)
+  }else{
+    array.push(product)
+  }
+  
+  setLocalStorage("so-cart", array);
 }
+
+
 // add to cart button event handler
 async function addToCartHandler(e) {
   const product = await dataSource.findProductById(e.target.dataset.id);
