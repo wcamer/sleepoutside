3ae206ffb17ec8608b36a1891f06b000e52acc8c
wcamer <xeus.xiii@hotmail.com>
2024-03-08 13:29:53 -0700

    code clean up

--- src/js/main.js
+++ src/js/main.js
@@ -2,7 +2,7 @@ import ProductData from "./ProductData.mjs";
 import ProductListing from "./ProductList.mjs";
 
 const pd = new ProductData("tents");
-const list = document.querySelector(".product-list")
-const pl = new ProductListing("tents",pd,list);
+const list = document.querySelector(".product-list");
+const pl = new ProductListing("tents", pd, list);
 
 pl.init();
