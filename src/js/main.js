20e0dd40f76020d4dcfe094072c2abd56805ff4f
wcamer <xeus.xiii@hotmail.com>
2024-03-08 13:14:51 -0700

    file creation

---
+++ src/js/main.js
@@ -0,0 +1,8 @@
+import ProductData from "./ProductData.mjs";
+import ProductListing from "./ProductList.mjs";
+
+const pd = new ProductData("tents");
+const list = document.querySelector(".product-list")
+const pl = new ProductListing("tents",pd,list);
+
+pl.init();
