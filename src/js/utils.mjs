8ac39e6cf5c41216378a7cf98752058679928649
wcamer <xeus.xiii@hotmail.com>
2024-03-08 13:13:59 -0700

    renderListWithTemplate function added and exported

--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -28,3 +28,18 @@ export function getParams(param){
   const product = urlParams.get('product')
   return product;
 }
+
+export function renderListWithTemplate(templateFn, parentElement, list, position = "afterbegin", clear = false) {
+  const htmlStrings = list.map(templateFn);
+  if(clear == true) {
+    
+    parentElement = "";
+  }
+  else{
+
+     parentElement.insertAdjacentHTML(position, htmlStrings.join(""));
+      
+  }
+ 
+
+}
