6ad3f39fdcd152fbf28cb24ddf6cca5de732ac40
wcamer <xeus.xiii@hotmail.com>
2024-03-16 18:39:13 -0600

    commenting out code since it is ran else where.  kept where it is for reference where it was.  Will soon delete comments

--- src/js/main.js
+++ src/js/main.js
@@ -3,8 +3,9 @@ import ProductListing from "./ProductList.mjs";
 import { loadHeaderFooter } from "./utils.mjs";
 
 loadHeaderFooter();
-const pD = new ProductData("tents");
-const list = document.querySelector(".product-list");
-const pL = new ProductListing("tents", pD, list);
+// const pD = new ProductData("tents");
+// const list = document.querySelector(".product-list");
+// const pL = new ProductListing("tents", pD, list);
 
-pL.init();
+// pL.init();
+//all moved to product-listing js
