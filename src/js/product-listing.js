f3a3be9b13e93cb11f2a8e467e99f69897b8c0d7
wcamer <xeus.xiii@hotmail.com>
2024-03-16 18:40:13 -0600

    file creation handling product listing index page

---
+++ src/js/product-listing.js
@@ -0,0 +1,20 @@
+import ProductData from "./ProductData.mjs";
+import ProductListing from "./ProductList.mjs";
+import { loadHeaderFooter , getParams} from "./utils.mjs";
+// console.log("11111111111111111111111111")
+loadHeaderFooter();
+const category = getParams("category")
+// console.log("666666666666666666666",category)
+const dataSource = new ProductData()
+// console.log("7777777777777777777",dataSource)
+
+
+//const pD = new ProductData("tents");
+const list = document.querySelector(".product-list");
+//const pL = new ProductListing("tents", pD, list);
+
+const myProdList = new ProductListing(category, dataSource, list)
+// console.log("88888888888888888888",myProdList)
+
+
+myProdList.init();
