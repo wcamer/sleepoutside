7df4ad4ab3be4cf15ff8a9aba9472ee1e6a37183
wcamer <xeus.xiii@hotmail.com>
2024-03-16 18:41:05 -0600

    w3 major reconstruction of ProductData class, getData function, and findProductById function

--- src/js/ProductData.mjs
+++ src/js/ProductData.mjs
@@ -1,4 +1,4 @@
-
+const baseURL = import.meta.env.VITE_SERVER_URL;
 
 function convertToJson(res) {
   if (res.ok) {
@@ -10,16 +10,28 @@ function convertToJson(res) {
 
 export default class ProductData {
   constructor(category) {
-    this.category = category;
-    this.path = `../json/${this.category}.json`;
+    ////Removed as per instructions
+    // this.category = category;
+    // this.path = `../json/${this.category}.json`;
   }
-  getData() {
-    return fetch(this.path)
-      .then(convertToJson)
-      .then((data) => data);
+
+  //updated per instructions
+async getData(category) {
+    // return fetch(this.path)
+    //   .then(convertToJson)
+    //   .then((data) => data);
+    const response = await fetch(baseURL + `products/search/${category}`)
+    const data = await convertToJson(response);
+    //console.log("3333333333333333",data)
+    return data.Result;
   }
+  //updated per instructions
   async findProductById(id) {
-    const products = await this.getData();
-    return products.find((item) => item.Id === id);
+    // const products = await this.getData();
+    // return products.find((item) => item.Id === id);
+    const response = await fetch(baseURL+ `product/${id}`)
+    const data =  await convertToJson(response)
+    //console.log("444444444444444",data,"\n555555555555555555555555",data.Result)
+    return data.Result;
   }
 }
